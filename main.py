455e1de5391051546bcbc11a65eff3fbeb5a0f65
hagay kohelet <hagay2506@gmail.com>
2025-11-04 17:49:46 +0200

    not finish

---
+++ main.py
@@ -0,0 +1,11 @@
+from classes.waiter import Waiter
+from classes.customer import Customer
+from classes.staff import Staff
+from classes.order import Order
+from classes.menu_item import MenuItem
+from classes.chef import Chef
+from classes.restaurant import Restaurant
+from classes.menu import Menu
+
+if __name__ == "__main__":
+    pass
