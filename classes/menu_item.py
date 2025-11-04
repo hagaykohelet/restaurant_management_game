2a72b59d15219cf2cc8b3bfc881dd18bacdf0c70
hagay kohelet <hagay2506@gmail.com>
2025-11-04 11:59:51 +0200

    menu_item

--- classes/menu_item.py
+++ classes/menu_item.py
@@ -1,3 +1,23 @@
 class MenuItem:
-    def __init__(self,name, price, category):
+    def __init__(self,name:str, price:int, category:str):
         self.name = name
+        self.price = price
+        self.category = category
+        self.available = True
+
+    def get_info(self):
+        return f"costumer: {self.name} price: {self.price} is order from {self.category}"
+
+    def set_available(self,status:bool):
+        self.available = status
+
+
+    def is_available(self):
+        return self.available
+
+
+
+
+s = MenuItem("hagay",21,"drink")
+s.set_available(False)
+print(s.is_available())
